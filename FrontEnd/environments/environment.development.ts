792c685dfeed2d5c600cf9db454be3dada57938e
horizonezodo <thiensutoiloi3@gmail.com>
2023-09-14 10:37:37 +0700

    Front End

---
+++ FrontEnd/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig :{
+        apiKey: "AIzaSyDbbU70L1wsKasn5ym0gZ36l5UUbPNAKnw",
+        authDomain: "springboot-angular-65f13.firebaseapp.com",
+        projectId: "springboot-angular-65f13",
+        storageBucket: "springboot-angular-65f13.appspot.com",
+        messagingSenderId: "481852468846",
+        appId: "1:481852468846:web:f7d01c2ddfa9d18261007f"
+    }
+};
